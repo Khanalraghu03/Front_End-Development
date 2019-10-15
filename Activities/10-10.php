ddcbe72694cdcbe8cdad1b1be2811ca2196282bc
Raghu Khanal <Khanalraghu03@gmail.com>
2019-10-15 10:17:54 -0400

    added everything up to 10-15

---
+++ Activities/10-10.php
@@ -0,0 +1,11 @@
+<?php
+	echo "Thank you! We will get back with you within 24-48 hours. <br><br>";
+
+	echo "Your first name: " . $_GET['first-name'] ."<br>";
+
+	echo "Your last name: " .$_GET['last-name']. "<br>";
+	echo "Your phone number: " .$_GET['p-number']. "<br>";
+	echo "Your email: " .$_GET['email']. "<br>";
+	echo "Your comment: " .$_GET['comments']. "<br>";
+
+?>
